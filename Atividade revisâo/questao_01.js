52558e5d15430f17cbaa6b421e74a69b52f1af43
Carlos-henrique-costa <62034240+Carlos-henrique-costa@users.noreply.github.com>
2021-08-24 20:10:11 -0300

    Delete Atividade revisâo directory

--- Atividade revisâo/questao_01.js
+++
@@ -1,5 +0,0 @@
-const prompt = require('prompt-sync')();
-
-const numero = Number(prompt('Digite o primeiro numero: '))
-
-console.log(`o antececor é: ${numero - 1} e o sucessor eh: ${numero + 1}`)
--- Atividade revisâo/questao_02.js
+++
@@ -1,7 +0,0 @@
-const prompt = require('prompt-sync')();
-
-const dolar = Number(prompt('Informe o valor em dolar: '))
-
-const real = (dolar * 5.42).toFixed(2)
-
-console.log(`O valor em real(R$) é: ${real}`)
